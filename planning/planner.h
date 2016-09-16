6d5eff08a091a76fe5872169952487acf15e196e
BerIgor <bigor11@gmail.com>
2016-09-16 08:49:36 -0700

    added the plannig thing

---
+++ planning/planner.h
@@ -0,0 +1,6 @@
+#include <ompl/geometric/SimpleSetup.h>
+
+bool isStateValid(const ompl::base::State *state);
+
+
+
---
+++ planning/test.cpp
@@ -0,0 +1,7 @@
+#include <base_local_planner/costmap_model.h>
+
+
+int main(){
+	printf("jefa\n");
+	return 0;
+}
